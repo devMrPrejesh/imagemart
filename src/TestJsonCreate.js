a229ed30804b76c1f4f3faf321d206d6a01487bf
devMrPrejesh <singhprejesh@gmail.com>
2023-01-27 18:22:21 +0530

    replace function

--- src/TestJsonCreate.js
+++ src/TestJsonCreate.js
@@ -6,7 +6,7 @@ let collections = fs.readdirSync(main_dir);
 
 testJson = {};
 collections.map(collection => {
-    let collection_images = fs.readdirSync(path.join(main_dir, collection)).map(img => "..\\" + path.join(main_dir, collection, img));
+    let collection_images = fs.readdirSync(path.join(main_dir, collection)).map(img => "../" + path.join(main_dir, collection, img).replaceAll("\\","/"));
     testJson[collection] = {collectionName: collection, count: collection_images.length, images: collection_images};
 });
 
